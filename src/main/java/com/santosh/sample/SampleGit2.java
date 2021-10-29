fe56eef2fceacc7998bb6f0d853f1f70eae58647
santosh <d.santosh.90@gmail.com>
2021-10-29 15:22:17 +0530

    First Commit by developer

---
+++ src/main/java/com/santosh/sample/SampleGit2.java
@@ -0,0 +1,12 @@
+package com.santosh.sample;
+
+public class SampleGit2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi am TL");
+		System.out.println("Hi am Developer");
+		
+	}
+
+}
